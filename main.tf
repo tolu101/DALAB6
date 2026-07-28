8b15b771e8073c58d06f9e899433f8ff7a0a3415
tolu101 <32836412+tolu101@users.noreply.github.com>
2026-07-28 14:30:17 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "<7f1c734d-bd3a-4224-84a5-fea5837caa5d>"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "jenkins-sa-mlsu72"
+    storage_account_name = "jenkinsstate-mlsu72"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-mlsu72-test-1"
+  location = "westeurope"
+}
